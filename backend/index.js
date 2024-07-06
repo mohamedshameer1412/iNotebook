f2975066701cca3f2f36a33b1b22081c127989c7
mohamedshameer1412 <75442892+mohamedshameer1412@users.noreply.github.com>
2024-07-07 05:03:32 +0530

    test5

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,14 @@ var cors = require('cors')
 
 connectToMongo();
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello, Vercel!');
+});
 // Available Routes
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
