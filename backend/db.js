bd80d6ef8d81f6ab5922a9baf6e6a43d7eca6adc
mohamedshameer1412 <75442892+mohamedshameer1412@users.noreply.github.com>
2024-07-07 19:53:13 +0530

    db test

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const mongoURI = process.env.MONGO_URI
-console.log(mongoURI)
+// console.log(mongoURI)
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI);
